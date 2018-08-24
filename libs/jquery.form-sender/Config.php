3f64328d3e8ba10dcd989ef581af4db6b2bccc3a
frontend_dimon <frontend@mmstudio.com.ua>
2018-08-24 23:44:09 +0300

    final

---
+++ libs/jquery.form-sender/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+    define("MAIL_METHOD", "MAIL"); // SMTP or MAIL
+    define("ORDER_MAIL_USERNAME", "contact@miner-tech.com");
+    define("ORDER_MAIL_HOST", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_PORT", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_PASSWORD", "~~~INSERT_HERE~~~");
+    define("ORDER_MAIL_CHARSET", "utf-8");
+    define("ORDER_MAIL_SENDER", "contact@miner-tech.com");
+
+    define("ORDER_MAIL_DEFAULT_TITLE", "Сообщение с сайта Miner Tech");
+    define("ORDER_MAIL_EMPTY_FIELD", "EMPTY FIELD");
+    define("ORDER_MAIL_EMPTY_VALUE", "EMPTY VALUE");
+?>
